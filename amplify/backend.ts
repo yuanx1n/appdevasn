5a838e5cc27d2623b92b482d272adcedaecedcc5
yuanx1n <liyuanxin682@gmail.com>
2025-02-05 03:41:30 +0800

    remove hardcoded userpool arn

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -15,15 +15,16 @@ const backend = defineBackend({
   postConfirmation,
 });
 
-
 // ✅ Attach Cognito Group Management Policy to PostConfirmation Lambda
 (() => {
   const postConfirmationLambda = backend.postConfirmation.resources.lambda;
+  const userPoolArn = backend.auth.resources.userPool.userPoolArn;
+  console.log("User Pool ARN:",userPoolArn);
 
   const cognitoPolicyStatement = new iam.PolicyStatement({
     effect: iam.Effect.ALLOW,
     actions: ["cognito-idp:AdminAddUserToGroup"],
-    resources: ["arn:aws:cognito-idp:us-east-1:314146339169:userpool/us-east-1_yr1ZwdDQN"], // Replace with actual values
+    resources: [userPoolArn],
   });
 
   postConfirmationLambda.addToRolePolicy(cognitoPolicyStatement);
